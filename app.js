caaaaf94ac70e63cb5c3a8bf72dc08fba5fa77f7
Sorabh <ssorabh.ssharma@gmail.com>
2017-09-28 14:24:41 +0530

    binding,directive,sce-whitelisted

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
 // MODULES
-var weatherForcastApp = angular.module('weatherForcastApp', ['ngRoute']);
+var weatherForcastApp = angular.module('weatherForcastApp', ['ngRoute', 'ngResource']);
 
-weatherForcastApp.config(function($routeProvider){
+weatherForcastApp.config(function($routeProvider, $sceDelegateProvider){
   $routeProvider.when('/', {
     templateUrl:'templates/main.html',
     controller: 'mainController'
   }).when('/forcast', {
     templateUrl:'templates/forcast.html',
     controller: 'forcastController'
+  }).when('/forcast/:days', {
+    templateUrl:'templates/forcast.html',
+    controller:'forcastController'
   });
+
+  // $sceProvider.enabled(false);
+  $sceDelegateProvider.resourceUrlWhitelist([
+    // Allow same origin resource loads.
+    'self',
+    // Allow loading from our assets domain.  Notice the difference between * and **.
+    'http://api.openweathermap.org/data/2.5/forecast**'
+  ]);
+  
+  // The blacklist overrides the whitelist so the open redirect here is blocked.
+  $sceDelegateProvider.resourceUrlBlacklist([
+    'http://myapp.example.com/clickThru**'
+  ]);
+  // console.log('sceProvider',$sceProvider);
 });
 
 // SERVICES
 weatherForcastApp.service('cityService', function(){
-  this.city = "Delhi, DL";
+  this.appId = "c629423cb43b0934aee9e92f67866836";
+  this.city = "Delhi, IN";
+  this.url = "http://api.openweathermap.org/data/2.5/forecast/";
+});
+
+// DIRECTIVES
+weatherForcastApp.directive("forcastElement",function(){
+  return {
+    restrict : "E",
+    templateUrl:"directives/forcast-element.html",
+    replace: true,
+    scope: {
+      weatherObj:"=",
+      convertDate:"&",
+      convertCelsius:"&",
+      convertFahrenheit:"&"
+    }
+  };
 });
 
 // CONTROLLERS
@@ -29,8 +63,25 @@ weatherForcastApp.controller('mainController',['$scope', '$location', 'cityServi
   });
   
 }]);
-weatherForcastApp.controller('forcastController',['$scope', 'cityService', function($scope, cityService){
+weatherForcastApp.controller('forcastController',['$scope', '$resource', '$routeParams', 'cityService', 
+  function($scope, $resource, $routeParams, cityService){
   $scope.city = cityService.city;
+  $scope.days = $routeParams.days || 2;
 
+  $scope.weatherAPI = $resource(cityService.url, { 
+    get:{ method:'JSONP' } 
+  });
+
+  $scope.weatherResult = $scope.weatherAPI.get({appid:cityService.appId, q:$scope.city, cnt:$scope.days});
+  
+  $scope.convertToDate = function(dt){
+    return new Date(dt*1000);
+  };
+  $scope.kelvin2Celsius = function(kel) {
+    return (kel - 273.15).toFixed(2);
+  };
+  $scope.kelvin2Fahrenheit = function(kel) {
+    return (((kel-273.15)*1.8)+32).toFixed(2);
+  }
 }]);
 
